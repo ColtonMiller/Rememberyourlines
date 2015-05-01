79177454491178ca3325335b9d7070193bc1a24e
Colton Miller <ColtonJMMiller@gmail.com>
2015-05-01 15:20:25 -0600

    Started

---
+++ Rememberyourlines/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//The original post is here: http://www.reddit.com/r/dailyprogrammer/comments/2xoxum/20150302_challenge_204_easy_remembering_your_lines/
+//The idea is to enter a bit of a line from macbeth and it shows the whole line 
+//I will be placing all the lines in a seperate region at the bottom
+namespace Rememberyourlines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    //make a class for line remembering
+    public class LineRemember
+    {
+        
+        //constructor that takes in write line as 
+
+    }
+//all text is in this region
+#region "Macbeth text"
+    public class MacBethText
+    {
+    }
+
+#endregion
+}
