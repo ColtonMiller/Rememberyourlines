fe212bc1df425ae71dc63c012965bfde21fe4496
Colton Miller <ColtonJMMiller@gmail.com>
2015-05-01 20:11:23 -0600

    reading .txt

--- Rememberyourlines/Program.cs
+++ Rememberyourlines/Program.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 //The original post is here: http://www.reddit.com/r/dailyprogrammer/comments/2xoxum/20150302_challenge_204_easy_remembering_your_lines/
 //The idea is to enter a bit of a line from macbeth and it shows the whole line 
 //I will be placing all the lines in a seperate region at the bottom
 namespace Rememberyourlines
 {
+
     class Program
     {
+        //make global string for entire .txt
+        public static List<string> listMacBeth = new List<string>{};
         static void Main(string[] args)
         {
+            GetMacBethText();
+            Console.ReadKey();
+        }
+        public static void GetMacBethText()
+        {
+            using (StreamReader reader = new StreamReader("MacBeth.txt"))
+            {
+                // Loop through the rest of the lines
+                while (!reader.EndOfStream)
+                {
+                    listMacBeth.Add(reader.ReadLine());
+                }
+            }
+            string stringMacBeth = string.Join("\n", listMacBeth);
+            string[] linesMacBeth = stringMacBeth.Split('\n');
+            
         }
     }
     //make a class for line remembering
@@ -21,11 +42,4 @@ namespace Rememberyourlines
         //constructor that takes in write line as 
 
     }
-//all text is in this region
-#region "Macbeth text"
-    public class MacBethText
-    {
-    }
-
-#endregion
 }
